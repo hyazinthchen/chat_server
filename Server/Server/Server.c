79f83c9ef1e1b9a301c8d9a23b9f55a0388a189b
Lena Goldmann <lenagoldmann@yahoo.de>
2021-01-02 16:19:23 +0100

    started implementing server functions

--- Server/Server/Server.c
+++ Server/Server/Server.c
@@ -1,104 +1,108 @@
-#define _CRT_SECURE_NO_WARNINGS
+//A simple server application using TCPv6-Sockets
+#pragma comment(lib, "Ws2_32.lib")
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <time.h>
-#include <winsock.h>
-#include <io.h>
-#pragma comment(lib, "Ws2_32.lib")
+#include <stdint.h>
+#include <Winsock2.h>
+#include <ws2tcpip.h>
 
-/* Portnummer */
-#define PORT 1234
+#define DEFAULT_FAMILY     AF_INET6     // Protocol family - in this case force IPv6
+#define DEFAULT_SOCKTYPE   SOCK_STREAM  // TCP uses SOCK_STREAM, UDP uses SOCK_DGRAM
+#define DEFAULT_PORT       1234         // The port for testing
+#define BUFFER_SIZE        1024         // The buffer size for the demonstation
 
-/* Puffer für eingehende Nachrichten */
-#define RCVBUFSIZE 1024
-
-static void echo(SOCKET);
-
-static void error_exit(char* errorMessage);
-
-/* Die Funktion gibt Daten vom Client auf stdout aus,
- * die dieser mit der Kommandozeile übergibt. */
-static void echo(SOCKET client_socket)
-{
-    char echo_buffer[RCVBUFSIZE];
+static void echo(SOCKET client_socket) {
+    char echo_buffer[BUFFER_SIZE];
     int recv_size;
-    time_t zeit;
+    time_t timestamp;
 
-    if ((recv_size =
-        recv(client_socket, echo_buffer, RCVBUFSIZE, 0)) < 0)
-        error_exit("Fehler bei recv()");
+    if ((recv_size = recv(client_socket, echo_buffer, BUFFER_SIZE, 0)) < 0) {
+        printf("\nError on receiving.\n");
+        exit(1);
+    }
+
     echo_buffer[recv_size] = '\0';
-    time(&zeit);
-    printf("Nachrichten vom Client : %s \t%s",
-        echo_buffer, ctime(&zeit));
-}
-
-/* Die Funktion gibt den aufgetretenen Fehler aus und
- * beendet die Anwendung. */
-static void error_exit(char* error_message) {
-    fprintf(stderr, "%s: %d\n", error_message, WSAGetLastError());
-    exit(EXIT_FAILURE);
+    time(&timestamp);
+    fprintf("\nMessage from client: %s \t%s", echo_buffer, ctime(&timestamp));
 }
 
 int main(int argc, char* argv[]) {
-    struct sockaddr_in server, client;
+    char buffer[BUFFER_SIZE];
+    int port = DEFAULT_PORT; //remove me when using next line of code
+    //char* port = atoi(argv[1]); //interpret port as int and not as string 
+    SOCKADDR_STORAGE From; //stores the IPv6 address information
+    int FromLen; //length of From
+    SOCKET ServerSocket;
+    struct sockaddr_in6 serv_addr;
 
-    SOCKET sock, fd;
-    unsigned int len;
+    //argc[0] = sNummer, argc[1] = portnumber where server listens argc < 2
+    if (argc < 1) {
+        fprintf(stderr, "Application needs sNumber and portnumber as arguments.\n");
+        exit(1);
+    }
+
+    printf("\nServer has been started.\n");
 
     /* Initialisiere TCP für Windows ("winsock"). */
     WORD wVersionRequested;
     WSADATA wsaData;
     wVersionRequested = MAKEWORD(1, 1);
-    if (WSAStartup(wVersionRequested, &wsaData) != 0)
-        error_exit("Fehler beim Initialisieren von Winsock");
-    else
-        printf("Winsock initialisiert\n");
-
-    /* Erzeuge das Socket. */
-    sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (sock < 0)
-        error_exit("Fehler beim Anlegen eines Sockets");
-
-    /* Erzeuge die Socketadresse des Servers. */
-    memset(&server, 0, sizeof(server));
-    /* IPv4-Verbindung */
-    server.sin_family = AF_INET;
-    /* INADDR_ANY: jede IP-Adresse annehmen */
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
-    /* Portnummer */
-    server.sin_port = htons(PORT);
-
-    /* Erzeuge die Bindung an die Serveradresse
-     * (genauer: an einen bestimmten Port). */
-    if (bind(sock, (struct sockaddr*)&server, sizeof(server)) < 0)
-        error_exit("Kann das Socket nicht \"binden\"");
-
-    /* Teile dem Socket mit, dass Verbindungswünsche
-     * von Clients entgegengenommen werden. */
-    if (listen(sock, 5) == -1)
-        error_exit("Fehler bei listen");
-
-    printf("Server bereit - wartet auf Anfragen ...\n");
-    /* Bearbeite die Verbindungswünsche von Clients
-     * in einer Endlosschleife.
-     * Der Aufruf von accept() blockiert so lange,
-     * bis ein Client Verbindung aufnimmt. */
-    for (;;) {
-        len = sizeof(client);
-        fd = accept(sock, (struct sockaddr*)&client, &len);
-        if (fd < 0)
-            error_exit("Fehler bei accept");
-        printf("Bearbeite den Client mit der Adresse: %s\n",
-            inet_ntoa(client.sin_addr));
-        /* Daten vom Client auf dem Bildschirm ausgeben */
-        echo(fd);
-
-        /* Schließe die Verbindung. */
-        closesocket(fd);
+    if (WSAStartup(wVersionRequested, &wsaData) != 0) {
+        printf("\nError on initialising Winsock.\n");
+        exit(1);
+    }   
+    else {
+        printf("\nWinsock initialised.\n");
     }
-    return EXIT_SUCCESS;
+
+    // Open a socket with the correct address family for this address.
+    ServerSocket = socket(DEFAULT_FAMILY, DEFAULT_SOCKTYPE, 0);
+    if (ServerSocket < 0) {
+        printf("\nError opening socket.\n");
+        exit(1);
+    }
+    else {
+        printf("\nSocket has been opened.\n");
+    }
+
+    memset((char*)&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin6_family = DEFAULT_FAMILY;
+    serv_addr.sin6_addr = in6addr_loopback;
+    serv_addr.sin6_port = htons(port);
+
+    // bind() associates a local address and port combination with the socket just created
+    if (bind(ServerSocket, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nError on binding socket.\n");
+        exit(1);
+    }
+    else {
+        printf("\nBinding successful.\n");
+    }
+
+    //listen() for incoming connections
+    if (listen(ServerSocket, 5) < 0) {
+        printf("\nError on listening.\n");
+        exit(1);
+    }
+    else {
+        printf("\nListening on port %d.\n", port);
+    }
+
+
+    //accept() a new connection
+    while (1) {
+        SOCKET NewSocket;
+        NewSocket = accept(ServerSocket, (LPSOCKADDR)&From, &FromLen);
+        if (NewSocket < 0) {
+            printf("\nError on accept.\n");
+            exit(1);
+        }
+
+        echo(ServerSocket);
+
+        //close() the socket
+        closesocket(NewSocket);
+    }
+    return 0;
 }
